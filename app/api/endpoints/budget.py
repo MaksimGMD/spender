8f6c3c2263dd3b61f138a4e1d1c43cab2d7aa3f7
Gritsuk Maksim <gritsuk@lanit.ru>
2024-03-14 01:24:49 +0300

    Add README.md

--- app/api/endpoints/budget.py
+++ app/api/endpoints/budget.py
@@ -14,6 +14,20 @@ NOT_FOUND_MESSAGE = "Бюджет не найден"
 
 @router.get("/{id}", response_model=BudgetSchema)
 def get_budget(*, session: SessionDep, current_user: CurrentUser, id: int):
+    """
+    **Получение информации о конкретном бюджете.**
+
+    Args:
+        session (SessionDep): Экземпляр сессии базы данных.
+        current_user (CurrentUser): Авторизованный пользователь.
+        id (int): Идентификатор бюджета.
+
+    Returns:
+        BudgetSchema: Информация о бюджете.
+
+    Raises:
+        HTTPException: Если бюджет не найден или пользователь пытается получить информацию о чужом бюджете.
+    """
     budget = crud.budget.get(session, id)
 
     if not budget:
@@ -27,12 +41,35 @@ def get_budget(*, session: SessionDep, current_user: CurrentUser, id: int):
 
 @router.get("/", response_model=List[BudgetSchema])
 def get_budgets(*, session: SessionDep, current_user: CurrentUser):
+    """
+    **Получение списка бюджетов пользователя.**
+
+    Args:
+        session (SessionDep): Экземпляр сессии базы данных.
+        current_user (CurrentUser): Авторизованный пользователь.
+
+    Returns:
+        List[BudgetSchema]: Список бюджетов пользователя.
+    """
     budgets = session.query(Budget).where(Budget.user_id == current_user.id).all()
     return budgets
 
 
 @router.post("/", response_model=BudgetSchema)
-def create_budget(*, session: SessionDep, budget_in: BudgetCreate, current_user: CurrentUser):
+def create_budget(
+    *, session: SessionDep, budget_in: BudgetCreate, current_user: CurrentUser
+):
+    """
+    **Создание нового бюджета.**
+
+    Args:
+        session (SessionDep): Экземпляр сессии базы данных.
+        budget_in (BudgetCreate): Данные для создания нового бюджета.
+        current_user (CurrentUser): Авторизованный пользователь.
+
+    Returns:
+        BudgetSchema: Созданный бюджет.
+    """
     budget = crud.budget.create(db=session, obj_in=budget_in, user_id=current_user.id)
     return budget
 
@@ -45,6 +82,21 @@ def update_budget(
     budget_id: int,
     budget_in: BudgetUpdate,
 ):
+    """
+    **Обновление информации о бюджете.**
+
+    Args:
+        session (SessionDep): Экземпляр сессии базы данных.
+        current_user (CurrentUser): Авторизованный пользователь.
+        budget_id (int): Идентификатор бюджета.
+        budget_in (BudgetUpdate): Обновленные данные для бюджета.
+
+    Returns:
+        BudgetSchema: Обновленная информация о бюджете.
+
+    Raises:
+        HTTPException: Если бюджет не найден или пользователь пытается изменить не свой бюджет.
+    """
     budget = crud.budget.get(session, budget_id)
     if not budget:
         raise HTTPException(status_code=404, detail=NOT_FOUND_MESSAGE)
@@ -64,6 +116,20 @@ def delete_budget(
     current_user: CurrentUser,
     budget_id: int,
 ):
+    """
+    Удаление бюджета.
+
+    Args:
+        session (SessionDep): Экземпляр сессии базы данных.
+        current_user (CurrentUser): Авторизованный пользователь.
+        budget_id (int): Идентификатор бюджета.
+
+    Returns:
+        str: Сообщение об успешном удалении бюджета.
+
+    Raises:
+        HTTPException: Если бюджет не найден или пользователь пытается удалить не свой бюджет.
+    """
     budget = crud.budget.get(session, budget_id)
     if not budget:
         raise HTTPException(status_code=404, detail=NOT_FOUND_MESSAGE)
